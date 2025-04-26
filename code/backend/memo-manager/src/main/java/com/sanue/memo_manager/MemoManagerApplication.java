443323f489ef63a7c92d7fc2c87e47d2fe9ba1ef
oukai <oukai0422@gmail.com>
2025-04-26 09:47:03 +0900

    feat:Spring Initializr を使用してプロジェクトを作成

---
+++ code/backend/memo-manager/src/main/java/com/sanue/memo_manager/MemoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sanue.memo_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoManagerApplication.class, args);
+	}
+
+}
